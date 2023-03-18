630382c8cfb43fe054dbf19363f1e468b49280bb
mohamedfffff <hdtsfr84@gmail.com>
2023-03-18 14:59:06 +0200

    add backend file

---
+++ backend.php
@@ -0,0 +1,11 @@
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+  Name: <input type="text" name="fname">
+  <input type="submit">
+</form>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // do logic
+  $name = $_POST['fname'];
+}
+?>
+
